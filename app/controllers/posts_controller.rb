0e893b9ee77b1b17c317e97bea52c9408c010d32
github username <github email address>
2019-03-15 12:12:55 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,13 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
+	end
+
+	private
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
 	end
 end
